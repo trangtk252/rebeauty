68ddf6c1e98a8d56cf6d558213f54f2432db24a4
J. van der Zande <jeffrey.van.der.zande@hva.nl>
2023-11-13 23:27:29 +0100

    New title and favicon

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
     reactStrictMode: true,
     images: {
         domains: ['botanistofficial.com', 'images-na.ssl-images-amazon.com', 'imgur.com']
