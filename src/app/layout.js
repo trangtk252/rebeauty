58d54323652f007819ebbb9b5c48983f56f0ae20
J. van der Zande <jeffrey.van.der.zande@hva.nl>
2023-11-13 23:15:02 +0100

    New title and favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,15 +6,14 @@ import Footer from "@/components/Footer";
 const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-    title: 'Rebeauty',
-    description: 'Rebeauty products',
+    title: 'ReBeauty',
+    description: 'Skin Care Product Review',
 }
 
 export default function RootLayout({children}) {
     return (
         <html lang="en">
         <head>
-            <title>ReBeauty - Skin Care Product Review</title>
             <meta charSet="utf-8"/>
             <meta httpEquiv="content-language" content="en-us"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
